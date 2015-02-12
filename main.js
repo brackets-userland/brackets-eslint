4385a431593d5ccd6f3aacc07038efee1bc17e5e
Martin Zagora <zaggino@gmail.com>
2015-02-12 18:59:30 +1100

    little refactoring

--- main.js
+++ main.js
@@ -13,6 +13,7 @@ define(function (require) {
     var YAML            = require("./js-yaml");
     var CONFIG_CACHE    = {};
     var DEBUG           = true;
+    var CONFIG_FILENAME = ".eslintrc";
     var jsLanguage      = LanguageManager.getLanguageForExtension("js");
 
     // register jsx and es6 as javascript file extensions in Brackets
@@ -33,17 +34,17 @@ define(function (require) {
             deferred.resolve(CONFIG_CACHE[fullPath]);
         };
 
-        FileSystem.resolve(fullPath, function (err, entry) {
+        FileSystem.resolve(fullPath, function (err1, entry) {
 
-            if (err || !entry.isFile) {
-                log("eslint: " + (err || "not-a-file") + " - " + fullPath);
+            if (err1 || !entry.isFile) {
+                log("eslint: " + (err1 || "not-a-file") + " - " + fullPath);
                 CONFIG_CACHE[fullPath] = null;
                 return finish();
             }
 
-            entry.read(function (err, content) {
+            entry.read(function (err2, content) {
 
-                if (err) {
+                if (err2) {
                     log("eslint: error reading contents - " + fullPath);
                     CONFIG_CACHE[fullPath] = null;
                     return finish();
@@ -76,6 +77,9 @@ define(function (require) {
         return deferred.promise();
     }
 
+    // this will split the path and return paths to follow ESLint spec:
+    // look for them in the directory of the file to be linted,
+    // and in successive parent directories all the way up to the root directory of the filesystem
     function getConfigFilePathsFromPath(fullPath) {
         var paths = [];
         var parts = fullPath.split("/");
@@ -85,35 +89,44 @@ define(function (require) {
 
         // now check all paths for a .eslintrc file
         while (parts.length > 0) {
-            paths.push(parts.join("/") + "/.eslintrc");
+            paths.push(parts.join("/") + "/" + CONFIG_FILENAME);
             parts.pop();
         }
 
         return paths;
     }
 
+    // method expects all config files to be already cached
     function getConfigForPathSync(fullPath) {
         var paths = getConfigFilePathsFromPath(fullPath);
         var defaultConfig = ESLint.defaults();
 
         var configs = _.compact(paths.map(function (path) {
-            return CONFIG_CACHE[path];
+            if (path in CONFIG_CACHE) {
+                return CONFIG_CACHE[path];
+            } else {
+                // path is not in cache so we should launch a cache refresh for this path
+                loadConfigFile(path);
+                return null;
+            }
         })).concat([defaultConfig]);
 
+        // this requires a two level defaults merge
         var result = {};
         Object.keys(defaultConfig).forEach(function (key) {
-            result[key] = _.defaults.apply(null, configs.map(function (config) {
-                return config[key] || {};
-            }));
+            result[key] = _.defaults.apply(null, _.compact(configs.map(function (config) {
+                return config[key] || null;
+            })));
         });
         return result;
     }
 
-    function getConfigForPath(fullPath) {
+    // load all config files into cache and then resolve in a sync way
+    function getConfigForPathAsync(fullPath) {
         var deferred = new $.Deferred();
         var paths = getConfigFilePathsFromPath(fullPath);
 
-        // load all files in paths if required
+        // load all files in paths if required (not in cache)
         var promises = _.compact(paths.map(function (path) {
             if (path in CONFIG_CACHE) {
                 return null;
@@ -138,6 +151,7 @@ define(function (require) {
         return deferred.promise();
     }
 
+    // this will map ESLint output to match format expected by Brackets
     function remapResults(results) {
         return {
             errors: results.map(function (result) {
@@ -165,20 +179,22 @@ define(function (require) {
     }
 
     function handleLintAsync(text, fullPath) {
-        return getConfigForPath(fullPath)
+        return getConfigForPathAsync(fullPath)
             .then(function (config) {
                 return doLint(text, fullPath, config);
             });
     }
 
+    // register a linter with CodeInspection
     CodeInspection.register(jsLanguage.getId(), {
         name: LINTER_NAME,
         scanFile: handleLintSync,
         scanFileAsync: handleLintAsync
     });
 
+    // watch for changes on config files and update the cache accordingly
     FileSystem.on("change", function (evt, entry) {
-        if (entry.isFile && entry.name === ".eslintrc") {
+        if (entry.isFile && entry.name === CONFIG_FILENAME) {
             loadConfigFile(entry.fullPath);
         }
     });
