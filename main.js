c84ffa09ffb75ceb11f619c01538a7195583e8cb
Martin Zagora <zaggino@gmail.com>
2015-02-13 13:57:53 +1100

    migration to node domain in progress

--- main.js
+++ main.js
@@ -1,165 +1,40 @@
 /*global $, brackets, console, define, setTimeout*/
 
-define(function (require) {
+define(function (require, exports, module) {
     "use strict";
 
-    var LINTER_NAME     = "ESLint";
+    // imports
     var _               = brackets.getModule("thirdparty/lodash");
     var CodeInspection  = brackets.getModule("language/CodeInspection");
-    var FileSystem      = brackets.getModule("filesystem/FileSystem");
-    var FileUtils       = brackets.getModule("file/FileUtils");
+    var ExtensionUtils  = brackets.getModule("utils/ExtensionUtils");
     var LanguageManager = brackets.getModule("language/LanguageManager");
-    var ESLint          = require("./eslint");
-    var YAML            = require("./js-yaml");
+    var NodeDomain      = brackets.getModule("utils/NodeDomain");
     var StateManager    = require("./StateManager");
-    var ExtensionInfo   = JSON.parse(require("text!./package.json"));
-    var CONFIG_CACHE    = {};
-    var DEBUG           = StateManager.get("DEBUG");
-    var CONFIG_FILENAME = ".eslintrc";
-    var jsLanguage      = LanguageManager.getLanguageForExtension("js");
+
+    // constants
+    var EXTENSION_DIR   = ExtensionUtils.getModulePath(module);
+    var JS_LANGUAGE     = LanguageManager.getLanguageForExtension("js");
+    var LINTER_NAME     = "ESLint";
+    var PACKAGE_JSON    = JSON.parse(require("text!./package.json"));
 
     // register jsx and es6 as javascript file extensions in Brackets
     ["es6", "jsx"].forEach(function (ext) {
-        if (!LanguageManager.getLanguageForExtension(ext)) { jsLanguage.addFileExtension(ext); }
+        if (!LanguageManager.getLanguageForExtension(ext)) { JS_LANGUAGE.addFileExtension(ext); }
     });
 
-    function log(str) {
-        if (DEBUG) {
-            console.log(ExtensionInfo.title + ": " + str);
-        }
-    }
-
-    function loadConfigFile(fullPath) {
-        var deferred = new $.Deferred();
-
-        var finish = function () {
-            deferred.resolve(CONFIG_CACHE[fullPath]);
-        };
-
-        FileSystem.resolve(fullPath, function (err1, entry) {
-
-            if (err1 || !entry.isFile) {
-                log((err1 || "not-a-file") + " - " + fullPath);
-                CONFIG_CACHE[fullPath] = null;
-                return finish();
-            }
-
-            entry.read(function (err2, content) {
-
-                if (err2) {
-                    log("error reading contents - " + fullPath);
-                    CONFIG_CACHE[fullPath] = null;
-                    return finish();
-                }
-
-                var config = null;
-
-                try {
-                    config = YAML.load(content);
-                } catch (e1) {
-                    log("cannot parse yaml - " + fullPath);
-                    try {
-                        config = JSON.parse(content);
-                    } catch (e2) {
-                        log("cannot parse json - " + fullPath);
-                    }
-                }
-
-                if (config) {
-                    log("file parsed successfully - " + fullPath);
-                }
-
-                CONFIG_CACHE[fullPath] = config;
-                return finish();
-
-            });
-
-        });
-
-        return deferred.promise();
-    }
-
-    // this will split the path and return paths to follow ESLint spec:
-    // look for them in the directory of the file to be linted,
-    // and in successive parent directories all the way up to the root directory of the filesystem
-    function getConfigFilePathsFromPath(fullPath) {
-        var paths = [];
-        var parts = fullPath.split("/");
-
-        // remove filename
-        parts.pop();
-
-        // now check all paths for a .eslintrc file
-        while (parts.length > 0) {
-            paths.push(parts.join("/") + "/" + CONFIG_FILENAME);
-            parts.pop();
-        }
-
-        return paths;
-    }
-
-    // method expects all config files to be already cached
-    function getConfigForPathSync(fullPath) {
-        var paths = getConfigFilePathsFromPath(fullPath);
-        var defaultConfig = ESLint.defaults();
-
-        var configs = _.compact(paths.map(function (path) {
-            if (path in CONFIG_CACHE) {
-                return CONFIG_CACHE[path];
-            } else {
-                // path is not in cache so we should launch a cache refresh for this path
-                loadConfigFile(path);
-                return null;
-            }
-        })).concat([defaultConfig]);
-
-        // this requires a two level defaults merge
-        var result = {};
-
-        var keys = _.uniq(_.flatten(configs.map(function (config) {
-            return Object.keys(config);
-        })));
-
-        keys.forEach(function (key) {
-            result[key] = _.defaults.apply(null, _.compact(configs.map(function (config) {
-                return config[key] || null;
-            })));
-        });
-
-        return result;
-    }
-
-    // load all config files into cache and then resolve in a sync way
-    function getConfigForPathAsync(fullPath) {
-        var deferred = new $.Deferred();
-        var paths = getConfigFilePathsFromPath(fullPath);
-
-        // load all files in paths if required (not in cache)
-        var promises = _.compact(paths.map(function (path) {
-            if (path in CONFIG_CACHE) {
-                return null;
-            } else {
-                return loadConfigFile(path);
-            }
-        }));
-
-        if (promises.length === 0) {
-            deferred.resolve(getConfigForPathSync(fullPath));
-        } else {
-            var finish = _.after(promises.length, function () {
-                deferred.resolve(getConfigForPathSync(fullPath));
-            });
-            promises.forEach(function (p) {
-                p.always(function () {
-                    finish();
-                });
-            });
-        }
-
-        return deferred.promise();
-    }
+    // setup a domain and install dependencies on startup
+    var nodeDomain = new NodeDomain("brackets-eslint", ExtensionUtils.getModulePath(module, "node/domain"));
+    // TODO: use which to resolve path to npm
+    nodeDomain.exec("executeAsync", EXTENSION_DIR, "c:/Program Files/nodejs/npm", ["install", "eslint"]).then(function (stdout) {
+        console.log("domain - ok");
+        console.log(stdout);
+    }, function (stderr) {
+        console.log("domain - err");
+        console.log(stderr);
+    });
 
     // this will map ESLint output to match format expected by Brackets
+    /*
     function remapResults(results) {
         return {
             errors: results.map(function (result) {
@@ -196,27 +71,11 @@ define(function (require) {
     }
 
     // register a linter with CodeInspection
-    CodeInspection.register(jsLanguage.getId(), {
+    CodeInspection.register(JS_LANGUAGE.getId(), {
         name: LINTER_NAME,
         scanFile: handleLintSync,
         scanFileAsync: handleLintAsync
     });
-
-    // watch for changes on config files and update the cache accordingly
-    FileSystem.on("change", function (evt, entry) {
-        if (entry && entry.isFile && entry.name === CONFIG_FILENAME) {
-            loadConfigFile(entry.fullPath);
-        }
-    });
-
-    // some helper methods to switch debug on/off through console
-    window[ExtensionInfo.name] = {
-        debug: function (bool) {
-            bool = !!bool;
-            StateManager.set("DEBUG", bool);
-            DEBUG = bool;
-            console.log(ExtensionInfo.title + ": DEBUG set to: " + bool);
-        }
-    };
+    */
 
 });
---
+++ node/domain.js
@@ -0,0 +1,55 @@
+(function () {
+    "use strict";
+
+    var ChildProcess  = require("child_process"),
+        domainName    = "brackets-eslint",
+        domainManager = null;
+
+    function fixEOL(str) {
+        if (str[str.length - 1] === "\n") {
+            str = str.slice(0, -1);
+        }
+        return str;
+    }
+
+    function executeAsync(directory, command, args, callback) {
+        // execute commands have to be escaped, spawn does this automatically and will fail if cmd is escaped
+        if (command[0] !== "\"" || command[command.length - 1] !== "\"") {
+            command = "\"" + command + "\"";
+        }
+        // http://nodejs.org/api/child_process.html#child_process_child_process_exec_command_options_callback
+        var toExec = command + " " + args.join(" ");
+        var child = ChildProcess.exec(toExec, {
+            cwd: directory,
+            maxBuffer: 20*1024*1024
+        }, function (err, stdout, stderr) {
+            callback(err ? fixEOL(stderr) : undefined, err ? undefined : fixEOL(stdout));
+        });
+    }
+
+    exports.init = function (_domainManager) {
+        domainManager = _domainManager;
+
+        if (!domainManager.hasDomain(domainName)) {
+            domainManager.registerDomain(domainName, { major: 0, minor: 1 });
+        }
+
+        domainManager.registerCommand(
+            domainName,
+            "executeAsync", // command name
+            executeAsync, // handler function
+            true, // is async
+            "executeAsync", // description
+            [
+                { name: "directory", type: "string" },
+                { name: "command", type: "string" },
+                { name: "args", type: "array" }
+            ],
+            [
+                { name: "stdout", type: "string" }
+            ]
+        );
+
+    };
+
+}());
