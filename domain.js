8c6becc2fdbb17d9be0ddb281575ae7f928f5d0c
Jan Matthias Pilzer <jan.matthias.pilzer@sap.com>
2015-03-13 14:18:12 +0100

    Add basic support for custom rules
    Rules have to be in a folder named .eslintrules in the project root

--- domain.js
+++ domain.js
@@ -5,12 +5,20 @@
   'use strict';
 
   var CLIEngine = require('eslint').CLIEngine;
-  var cli = new CLIEngine();
+  var cli;
   var domainName = 'zaggino.brackets-eslint';
   var domainManager = null;
 
   function lintFile(fullPath) {
     return cli.executeOnFiles([fullPath]);
+  }
+
+  function setProjectRoot(projectRoot) {
+    cli = new CLIEngine({
+      rulePaths: [
+        projectRoot + '.eslintrules'
+      ]
+    });
   }
 
   exports.init = function (_domainManager) {
@@ -38,6 +46,20 @@
         {
           name: 'report',
           type: 'object'
+        }
+      ]
+    );
+
+    domainManager.registerCommand(
+      domainName,
+      'setProjectRoot', // command name
+      setProjectRoot, // handler function
+      false, // is not async
+      'Set the rulePaths of ESLint CLIEngine', // description
+      [
+        {
+          name: 'projectRoot',
+          type: 'string'
         }
       ]
     );
